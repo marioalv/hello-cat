575ec99268c9ac1810350ea44ac4e722d9824c42
Mario Alvarado <mario@appcoast.com>
2018-12-14 11:19:26 -0600

    First set of changes for the Hello Cat repository

---
+++ src/background/background.js
@@ -0,0 +1,36 @@
+if (typeof msBrowser !== 'undefined') {
+  chrome = msBrowser;
+}
+else if (typeof browser != 'undefined')
+{
+  chrome = browser;
+}
+
+chrome.browserAction.onClicked.addListener(function (activeTab) {
+  chrome.tabs.create({ url: 'background/helloCat.html' }, null);
+  google.payments.inapp.getPurchases({
+    'parameters': {'env': 'prod'},
+    'success': getPurchasesSuccess,
+    'failure': getPurchasesSuccess
+  });
+});
+
+
+console.log("Hello Cat");
+
+google.payments.inapp.getPurchases({
+  'parameters': {'env': 'prod'},
+  'success': getPurchasesSuccess,
+  'failure': getPurchasesSuccess
+});
+
+function getPurchasesSuccess(aResponse) {
+  console.log(" +++ getPurchases SUCCESS +++ ");
+  console.log(aResponse);
+
+}
+
+function getPurchasesSuccess(aResponse) {
+  console.log(" +++ getPurchases FAILURE +++ ");
+  console.log(aResponse);
+}
